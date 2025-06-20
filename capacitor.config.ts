0500d31bfe303ef322e6062a2737174b8b76dd7b
Artur <artur.miroschin@gmail.com>
2025-06-20 14:56:53 +0200

    base

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.day0',
+  appName: 'Day0',
+  webDir: 'dist'
+};
+
+export default config;
